c8f355202357d66f3779cc2764a5d82cb4aec546
hyunn00 <eorn21301@gmail.com>
2024-01-29 09:44:10 +0900

    user entity 정리

--- src/main/java/com/yi/spring/controller/IndexController.java
+++ src/main/java/com/yi/spring/controller/IndexController.java
@@ -1,8 +1,6 @@
 package com.yi.spring.controller;
 
-import com.yi.spring.service.UserService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
-
-    @Autowired
-    UserService userService;
-
 
     @GetMapping("/home")
     public String index(Model model, HttpSession httpSession) {
