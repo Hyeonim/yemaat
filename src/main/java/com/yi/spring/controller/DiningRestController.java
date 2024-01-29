0a86dc8bd4ad2e8ab58f1251439865252f6d8042
hyunn00 <eorn21301@gmail.com>
2024-01-29 14:22:28 +0900

    dining_rest CRUD 기능 구현(html x)

--- src/main/java/com/yi/spring/controller/DiningRestController.java
+++ src/main/java/com/yi/spring/controller/DiningRestController.java
@@ -2,12 +2,10 @@ package com.yi.spring.controller;
 
 import com.yi.spring.entity.DiningRest;
 import com.yi.spring.service.DiningRestService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
