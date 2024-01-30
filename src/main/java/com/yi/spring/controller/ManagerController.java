980753337dad285fe11ac31cb623bc4e3fc89b44
Hyeonim <abc105484@naver.com>
2024-01-30 17:53:26 +0900

    m

--- src/main/java/com/yi/spring/controller/ManagerController.java
+++ src/main/java/com/yi/spring/controller/ManagerController.java
@@ -1,35 +1,52 @@
 package com.yi.spring.controller;
 
-import com.yi.spring.entity.Dinning;
+import com.yi.spring.entity.User;
+import com.yi.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class ManagerController {
 
-
-//    @GetMapping("/")
-//    public String manager(Model model) {
-//        return "managerPage";
-//    }
+    @Autowired
+    UserService userService;
 
     @GetMapping("/{subPage}")
     public String managerPage(Model model, @PathVariable String subPage) {
+
         model.addAttribute( "page", "managerPage/"+subPage );
+
+
+        List<User> users = userService.getAllUsers();
+        System.out.println(users);
+        model.addAttribute("users",users);
+
         return "managerPage";
     }
 
-//    @GetMapping("/managerInfoPage")
-//    public String managerInfoPage(Model model) {
-//        // 필요한 로직 추가
-//        return "managerInfoPage"; // managerInfoPage.html을 반환
-//    }
+    @GetMapping("/aa/managerPage_UList")
+    public String managerUList(Model model, @PathVariable String subPage) {
+
+        model.addAttribute( "page", "managerPage/"+subPage );
+
+
+        List<User> users = userService.getAllUsers();
+        System.out.println(users);
+        model.addAttribute("users",users);
+
+        return "managerPage";
+
+    }
+
+
+
+
 
 }
