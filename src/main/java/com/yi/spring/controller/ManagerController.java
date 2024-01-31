4bb1dc35ab1b3acc2e6185429ce427e1db7d283e
Hyeonim <abc105484@naver.com>
2024-01-31 17:32:56 +0900

    manager-User_add

--- src/main/java/com/yi/spring/controller/ManagerController.java
+++ src/main/java/com/yi/spring/controller/ManagerController.java
@@ -6,10 +6,10 @@ import com.yi.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,28 +30,52 @@ public class ManagerController {
         return "managerPage";
     }
 
+//    ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ유저꺼ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
     @GetMapping("/managerPage_UList")
     public String managerListU(Model model) {
 
-        model.addAttribute( "page", "managerPage/managerPage_UList" );
+
         List<User> users = userRepository.findAll();
-
         List<User> onlyUsers = new ArrayList<>();
-
         for (User result : users) {
             if (result.getUserAuth().equals("1")) {
                 onlyUsers.add(result);
             }
         }
-
-        System.out.println(users);
         model.addAttribute("users",onlyUsers);
 
+
+        model.addAttribute( "page", "managerPage/managerPage_UList" );
         return "managerPage";
-
     }
+//
+//
+//    @PostMapping("managerPage_UAdd")
+//    public String userAdd(@RequestParam MultipartFile file, User user,Model model) {
+//
+//        model.addAttribute( "page", "managerPage/managerPage_UList" );
+//
+//        if (file.isEmpty()) {
+//            userRepository.save(user);
+//        } else {
+//            byte[] userImg = new byte[0];
+//            try {
+//                userImg = file.getBytes();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//            user.setUserImg(userImg);
+//        }
+//
+//        userRepository.save(user);
+//
+//        return "/managerPage";
+//    }
 
+
+
+//    ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ점주꺼ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
     @GetMapping("/managerPage_JInfo")
     public String managerInfoA(Model model) {
