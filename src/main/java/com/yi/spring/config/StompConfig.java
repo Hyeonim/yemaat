424dad9000115ccb88b1c5320f3edd07db485db9
김영맨33 <jangyhun@naver.com>
2024-02-02 21:32:03 +0900

    서버 설치

--- src/main/java/com/yi/spring/config/StompConfig.java
+++ src/main/java/com/yi/spring/config/StompConfig.java
@@ -1,4 +1,5 @@
 package com.yi.spring.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,6 +9,20 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+//    @Value("${spring.rabbitmq.port}")
+    @Value("${server.port}")
+    private int port;
+    @Value("${spring.rabbitmq.virtualHost}")
+    private String virtualHost;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -23,7 +38,12 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub");
 
         //registry.enableSimpleBroker("/sub");
-        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+                .setRelayHost(host)
+//                .setRelayPort(port)
+                .setClientLogin(username)
+                .setClientPasscode(password)
+        ;
     }
 
 }
