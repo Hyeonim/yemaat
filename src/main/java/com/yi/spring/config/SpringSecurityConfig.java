8f11361b8797254297cd11e2238808c42753f8f7
김영맨33 <jangyhun@naver.com>
2024-02-07 19:13:48 +0900

    로그인 인증 필요하도록 세팅 user owner manager

---
+++ src/main/java/com/yi/spring/config/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.yi.spring.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request->request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/status","/images/**","/css/**","/js/**","/view/join","/auth/join").permitAll()
+                        .requestMatchers( "/", "/home").permitAll()
+                        .requestMatchers("/user/**").hasAnyRole("1", "2", "3")
+                        .requestMatchers("/owner/**").hasAnyRole("2", "3")
+                        .requestMatchers("/manager/**").hasRole("3")
+                        .anyRequest().authenticated()
+                ).formLogin(login->login
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login-process")
+                        .usernameParameter("userId")
+                        .passwordParameter("userPassword")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                ).logout(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+        return new RawPasswordEncoder();
+    }
+}
