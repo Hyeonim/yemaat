19ef49687b5e7b827a3118748c36b901f9da5bc3
Hyeonim <abc105484@naver.com>
2024-02-27 10:21:52 +0900

    login - email 인증 완

---
+++ src/main/java/com/yi/spring/controller/MailController.java
@@ -0,0 +1,33 @@
+package com.yi.spring.controller;
+
+import com.yi.spring.entity.MailVo;
+import com.yi.spring.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    @GetMapping("/CheckMail")
+    public String MailPage(){
+        return "View";
+    }
+
+    @ResponseBody
+    @PostMapping("/mail")
+    public String MailSend(String mail){
+
+        int number = mailService.sendMail(mail);
+
+        String num = "" + number;
+
+        return num;
+    }
+
+}
