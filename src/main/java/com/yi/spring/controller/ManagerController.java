98f688d959af953310bf2bd86fda3921630b2386
Hyeonim <abc105484@naver.com>
2024-02-28 17:22:02 +0900

    managerPage - 수정

--- src/main/java/com/yi/spring/controller/ManagerController.java
+++ src/main/java/com/yi/spring/controller/ManagerController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
